2559b1ede70e9ebd0a57a5c114f358b4e1d0bebd
Fabio Barbosa <fabio.barbosa@gmail.com>
2023-09-28 22:16:44 -0300

    add color parameter to the options

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"image"
 	"image/color"
 	"net/http"
@@ -20,6 +21,7 @@ type EntradaImagem struct {
 	XPosition float64 `json:"xPosition"`
 	YPosition float64 `json:"yPosition"`
 	FontSize  float64 `json:"fontSize"`
+	FontColor string  `json:"fontColor"`
 }
 
 func main() {
@@ -59,9 +61,17 @@ func main() {
 
 		//creates a intance of the image editor
 		dc := gg.NewContextForImage(i)
-		dc.SetColor(color.White)
 
 		for _, ei := range req.ListaEntradas {
+
+			var selectedColor color.Color
+			if selectedColor, err = parseHexColor(ei.FontColor); err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": "invalid hex color code"})
+				return
+			}
+
+			dc.SetColor(selectedColor)
+
 			if err = dc.LoadFontFace("fonts/OpenSans-VariableFont_wdth,wght.ttf", ei.FontSize); err != nil {
 				ctx.JSON(500, gin.H{"error": "Error loading font"})
 				return
@@ -80,4 +90,16 @@ func main() {
 
 	r.Run(":8080")
 
+}
+
+func parseHexColor(hexColor string) (color.RGBA, error) {
+	// Parse the hex color code using the color.RGBA constructor
+	c := color.RGBA{}
+	_, err := fmt.Sscanf(hexColor, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+	if err != nil {
+		return c, err
+	}
+	// Set alpha channel to 255 (fully opaque)
+	c.A = 255
+	return c, nil
 }
